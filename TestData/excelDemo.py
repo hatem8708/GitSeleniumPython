c5a9a3d9102c8e58203d3d4577b34150bb218085
Hatem <hatem.souabni@auticonsult.fr>
2020-11-04 10:49:59 +0100

    African timezone commit

--- TestData/excelDemo.py
+++ TestData/excelDemo.py
@@ -31,3 +31,10 @@ for i in range(1, sheet.max_row+1):
         for j in range(2, sheet.max_column+1):
             Dict[sheet.cell(row=1, column=j).value]= sheet.cell(row=i, column=j).value
 print(Dict)
+
+
+# sùglùdfgmfdgmùd
+
+# fsdfsmgùdfg
+
+# lùfsdgùslgdfùm
